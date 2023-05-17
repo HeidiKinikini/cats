49852fdf2c134af0a5a078f74662e69cd87d301e
Heidi Kinikini <Duckwitz52@gmail.com>
2023-05-17 12:24:49 -0600

    serves frontend with express.static instead of indv. endpoints

--- public/server.js
+++ public/server.js
@@ -4,17 +4,19 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/main', (req, res) => {
-    res.sendFile(__dirname + '/public/main.js')
-})
+app.use(express.static(__dirname + '/public'))
 
-app.get('/styles', (req, res) => {
-    res.sendFile(__dirname + '/public/styles.css')
-})
+//app.get('/main', (req, res) => {
+//    res.sendFile(__dirname + '/public/main.js')
+//})
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
-})
+//app.get('/styles', (req, res) => {
+//    res.sendFile(__dirname + '/public/styles.css')
+//})
+
+//app.get('/', (req, res) => {
+//    res.sendFile(__dirname + '/public/index.html')
+//})
 
 app.listen(4000, () => {
     console.log('Up on 4000')
